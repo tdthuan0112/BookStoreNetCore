cb4872923dd6804e76c7d0839cad2027e5c336f9
Thuan Tran <trandinhthuan0112@gmail.com>
2025-04-24 11:01:49 +0700

    update admin navigation

--- Frontend/bookstoreecommerce.client/components/admin/admin-navigation.jsx
+++ Frontend/bookstoreecommerce.client/components/admin/admin-navigation.jsx
@@ -9,6 +9,8 @@ import viewListImg from "@/assets/img/view-list.png";
 import bookEditImg from "@/assets/img/book-edit.png";
 import userEditImg from "@/assets/img/user-edit.png";
 import categoryEditImg from "@/assets/img/category.png";
+import appUsersRoles from "@/assets/img/app-users-roles.png";
+
 import classes from "@/styles/common/admin-navigation.module.css";
 
 export default function AdminNavigation() {
@@ -17,6 +19,56 @@ export default function AdminNavigation() {
   const isManageBookPage = pathname === "/admin/manage-books";
   const isManageUserPage = pathname === "/admin/manage-users";
   const isManageCategoryPage = pathname === "/admin/manage-categories";
+  const isManageRolePage = pathname === "/admin/manage-roles";
+
+  const navigationItems = [
+    {
+      key: 1,
+      name: "manageOrderPage",
+      href: "/admin/manage-orders",
+      isActive: isManageOrderPage,
+      icon: viewListImg,
+      alt: "Account Image",
+      displayText: "Manage Orders"
+    },
+    {
+      key: 2,
+      name: "manageBookPage",
+      href: "/admin/manage-books",
+      isActive: isManageBookPage,
+      icon: bookEditImg,
+      alt: "Book Edit Image",
+      displayText: "Manage Books"
+    },
+    {
+      key: 3,
+      name: "manageUserPage",
+      href: "/admin/manage-users",
+      isActive: isManageUserPage,
+      icon: userEditImg,
+      alt: "User Edit Image",
+      displayText: "Manage Users"
+    },
+    {
+      key: 4,
+      name: "manageCategoryPage",
+      href: "/admin/manage-categories",
+      isActive: isManageCategoryPage,
+      icon: categoryEditImg,
+      alt: "Category Edit Image",
+      displayText: "Manage Categories"
+    },
+    {
+      key: 5,
+      name: "manageRolePage",
+      href: "/admin/manage-roles",
+      isActive: isManageRolePage,
+      icon: appUsersRoles,
+      alt: "Role Edit Image",
+      displayText: "Manage Roles"
+    },
+  ]
+
   return (
     <div>
       <div>
@@ -25,47 +77,17 @@ export default function AdminNavigation() {
           <strong>Thuan Tran</strong>
         </p>
       </div>
-      <Link
-        href="/admin/manage-orders"
-        className={`${classes.navLink} ${
-          isManageOrderPage ? classes.activeLink : undefined
-        }`}
-      >
-        <Image src={viewListImg} with={20} height={20} alt="Account Image" />
-        <p>Manage Orders</p>
-      </Link>
-      <Link
-        href="/admin/manage-books"
-        className={`${classes.navLink} ${
-          isManageBookPage ? classes.activeLink : undefined
-        }`}
-      >
-        <Image src={bookEditImg} with={20} height={20} alt="Account Image" />
-        <p>Manage Books</p>
-      </Link>
-      <Link
-        href="/admin/manage-users"
-        className={`${classes.navLink} ${
-          isManageUserPage ? classes.activeLink : undefined
-        }`}
-      >
-        <Image src={userEditImg} with={20} height={20} alt="Account Image" />
-        <p>Manage Users</p>
-      </Link>
-      <Link
-        href="/admin/manage-categories"
-        className={`${classes.navLink} ${
-          isManageCategoryPage ? classes.activeLink : undefined
-        }`}
-      >
-        <Image
-          src={categoryEditImg}
-          with={20}
-          height={20}
-          alt="Account Image"
-        />
-        <p>Manage Categories</p>
-      </Link>
+      {navigationItems.map(item => (
+        <Link
+          key={item.key}
+          href={item.href}
+          className={`${classes.navLink} ${item.isActive ? classes.activeLink : undefined
+            }`}
+        >
+          <Image src={item.icon} with={20} height={20} alt={item.alt} />
+          <p>{item.displayText}</p>
+        </Link>
+      ))}
     </div>
   );
 }
